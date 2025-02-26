b7daaf24ef81dd13b5d469c4d3cf932987dd62fe
Santhoshkumar450 <santhoshsandy1612@gmail.com>
2025-02-26 18:13:15 +0530

    check the sample

--- src/demo_gitprj/Samp_git1.java
+++ src/demo_gitprj/Samp_git1.java
@@ -2,4 +2,18 @@ package demo_gitprj;
 
 public class Samp_git1 {
 
+	private void sample1() {
+		System.out.println("Enter the valid data");
+
+	}
+	private void sample2() {
+		// TODO Auto-generated method stub
+System.out.println("Enter the Invalid data");
+	}
+	public static void main(String[] args) {
+		
+		Samp_git1 s = new Samp_git1();
+				s.sample1();
+				s.sample2();
+	}
 }
